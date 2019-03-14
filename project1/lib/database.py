ce6cac5e9a11baf63ffa8b93a0bd645fc78d239c
akio-outori <jhallyburton@aquent.com>
2019-03-13 20:20:04 -0400

    updates to further abstract the database layer

---
+++ project1/lib/database.py
@@ -0,0 +1,30 @@
+import psycopg2
+
+class database(object):
+    """
+    Create the postgres database connection and return the connection object
+    """
+
+    def connection(db, user, host):
+        connection_parameters = {
+            'dbname': db,
+            'user': user,
+            'host': host
+        }
+        try:
+            return psycopg2.connect(**connection_parameters)
+        except:
+            print("I am unable to connect to the database")
+            conn.close()
+
+    def query(connection, query):
+        try:
+            cursor = connection.cursor()
+            cursor.execute(query)
+            return cursor.fetchall()
+        except Exception as error:
+            message = "Error! unable to run query: " + query
+            print(message)
+            print(error)
+        finally:
+            connection.close()
--- project1/report.py
+++ project1/report.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python
 
-import psycopg2
+# Reporting script to return the required values from the postgres database.
+# Boilerplate code borrowed from this project - https://github.com/akio-outori/terraform-renderer
 
-def create_connection(db, user, host):
-    connection_parameters = {
-        'dbname': db,
-        'user': user,
-        'host': host
-    }
-    try:
-        return psycopg2.connect(**connection_parameters)
-    except:
-        print("I am unable to connect to the database")
-        conn.close()
+import sys
+from lib.switch import switch
+from lib.database import database
 
-def exec_query(conn, query):
-    cursor = conn.cursor()
-    cursor.execute(query)
+# Show possible command line options
+def showOpts():
+  print("Option not understood:")
+  print("  top_authors")
+  print("  top_articles")
+  print("\nusage: tf-generator <option> <file>.yml")
+  sys.exit(1)
 
-    return cursor.fetchall()
+def run_query(query):
+    db = database.connection('news', 'postgres', 'localhost')
+    return database.query(db, query)
 
+# Get Options
 try:
-    connection = create_connection('news', 'postgres', 'localhost')
-    results = exec_query(connection, "select * from articles;")
-    print(results)
+    option = sys.argv[1]
 except:
-    print("unable to return query results")
-finally:
-    connection.close()
+    showOpts()
+
+for case in switch(option):
+
+    if case("top_authors"):
+        print(run_query('select * from top_authors limit 3;'))
+        break
+
+    if case("top_articles"):
+        print(run_query('select * from top_articles;'))
+        break
